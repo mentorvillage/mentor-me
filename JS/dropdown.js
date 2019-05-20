5f10207c96ed246dcec2737529b1a4a1711c5452
Melvin Peepers <peepersm@gmail.com>
2019-05-20 18:51:27 -0400

    Most of the way done with Mobile, Tablet and Desktop views

---
+++ JS/dropdown.js
@@ -0,0 +1,14 @@
+// let nav = document.getElementById('js-menu');
+// let navBarToggle = document.getElementById('js-navbar-toggle');
+
+// navBarToggle.addEventListener('click', function() {
+//     nav.classList.toggle('active');
+// });
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener('click', function () {
+    
+    mainNav.classList.toggle('active');
+});
